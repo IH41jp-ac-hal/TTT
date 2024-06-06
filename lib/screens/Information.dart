88d8ac1ffc4f14177e20b8dd798f8c932ac202a4
Kenji-priv46 <ebikenn12@gmail.com>
2024-06-06 17:36:25 +0900

    混雑状況ページ追加

--- lib/screens/Information.dart
+++ lib/screens/Information.dart
@@ -9,9 +9,42 @@ class InformationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TruckerTrekker',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(110.0),
+          child: AppBar(
+            centerTitle: false,
+            title: Text(
+              '配送状況',
+              style: TextStyle(fontSize: 19, height: 4),
+            ),
+            backgroundColor: Color.fromARGB(255, 9, 142, 163),
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 15.0, top: 23.0),
+                child: Container(
+                  width: 114,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage('assets/logo.png'), //画像のパス指定
+                      )),
+                ),
+              )
+            ],
+          ),
+        ),
+        body: Container(
+          width: MediaQuery.of(context).size.width, // 画面の幅を設定
+          height: MediaQuery.of(context).size.height * 0.4, // 画面の高さを設定
+          color: Color.fromARGB(255, 201, 246, 0),
+          child: Text(
+            '混雑状況',
+            style:
+                TextStyle(fontSize: 44, height: 4, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
     );
   }
