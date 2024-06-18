6aeae3e6d784b3ecc15e66612c28159c04012cb1
ths10828mizunowataru <run.hinode@gmail.com>
2024-06-18 22:56:00 +0900

    firebase 初期設定

--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -1,25 +1,46 @@
-import 'package:flutter/material.dart';
-import 'package:trukkertrakker/src/app.dart';
+// import 'package:flutter/material.dart';
+// import 'package:trukkertrakker/src/app.dart';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCYo7gqY3tezdXmfVbkypRd2eVZXPudPGc",
-  authDomain: "ih12b213-4.firebaseapp.com",
-  databaseURL: "https://ih12b213-4-default-rtdb.firebaseio.com",
-  projectId: "ih12b213-4",
-  storageBucket: "ih12b213-4.appspot.com",
-  messagingSenderId: "988142203119",
-  appId: "1:988142203119:web:eea1669d6d6401350e2b1a",
-  measurementId: "G-C80M7XW650"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCYo7gqY3tezdXmfVbkypRd2eVZXPudPGc",
+//   authDomain: "ih12b213-4.firebaseapp.com",
+//   databaseURL: "https://ih12b213-4-default-rtdb.firebaseio.com",
+//   projectId: "ih12b213-4",
+//   storageBucket: "ih12b213-4.appspot.com",
+//   messagingSenderId: "988142203119",
+//   appId: "1:988142203119:web:eea1669d6d6401350e2b1a",
+//   measurementId: "G-C80M7XW650"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+
+// await Firebase.initializeApp(
+//   options: DefaultFirebaseOptions.currentPlatform,
+// );
+
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+
+  runApp(
+    const MyApp(),
+  );
+}
