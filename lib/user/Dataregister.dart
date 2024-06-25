9e3bfe8707afd3c8c399b5276d741ef6d9cd35f3
wataru <w97715@gmail.com>
2024-06-25 17:07:31 +0900

    ファイル作成

---
+++ lib/user/Dataregister.dart
@@ -0,0 +1,11 @@
+
+
+// void write(WidgetRef ref) async {
+//   try {
+//     await dbRef.update({
+//       '$userID/count': ref.read(counterProvider),
+//     });
+//   } catch (e) {
+//     print('Error : $e');
+//   }
+// }
